08ce13b77c68d0d90e5ae900c5e1ec489cc133a2
Jack Aboutboul <jack@jackfoundation.com>
2012-05-30 11:14:56 -0400

    re-add credentials.php after filter/rebase

---
+++ php/credentials.php
@@ -0,0 +1,12 @@
+<?php
+
+//your twilio account sid from the dashboard
+$ACCOUNT_SID = '';
+
+//your twilio auth token from the dashboard
+$AUTH_TOKEN  = '';
+
+//a twilio number which you've purchased FORMAT: +1XXXXXXXXXX (No parentheses, spaces or dashes)
+$TWILIO_NUMBER ='';
+
+?>
